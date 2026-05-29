f1f5077c2c6730c070b84427bab2941cac726c92
AegisAX <yjjeon.whitehat@gmail.com>
2026-05-30 02:35:00 +0900

    fix(phish): X-Server 헤더 노출 제거 (핑거프린팅 방지)
    피싱 서버가 도구 정체를 헤더로 광고하던 X-Server 제거.
    SEG/수신자의 시뮬레이션 탐지 표면 축소.

--- controllers/phish.go
+++ controllers/phish.go
@@ -612,7 +612,6 @@ func (ps *PhishingServer) PhishHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Header().Set("X-Server", config.ServerName) // Useful for checking if this is a Sentinel server (e.g. for campaign reporting plugins)
 	var ptx models.PhishingTemplateContext
 	// Check for a preview
 	if preview, ok := ctx.Get(r, "result").(models.EmailRequest); ok {
